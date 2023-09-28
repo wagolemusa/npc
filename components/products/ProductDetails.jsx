e352b2946c296399e531d676a306b7e3d9719b90
wagolemusa <homiemusa@gmail.com>
2023-09-28 14:02:35 +0300

    Worked on Cart

--- components/products/ProductDetails.jsx
+++ components/products/ProductDetails.jsx
@@ -1,10 +1,15 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useContext } from "react";
 import StarRatings from "react-star-ratings";
 import BreadCrumbs from "../layouts/BreadCrumbs";
+import CartContext from '../../context/CartContext'
+
 
 const ProductDetails = ({ product }) => {
+
+  const { addItemToCart } = useContext(CartContext)
+  
   const imgRef = useRef(null);
 
   const setImgPreview = (url) => {
@@ -20,6 +25,19 @@ const ProductDetails = ({ product }) => {
       url: `/products/${product?._id}`,
     },
   ];
+
+  // Add to cart function
+  const addToCartHandler = () => {
+    addItemToCart({
+      product: product._id,
+      name: product.name,
+      price: product.price,
+      images: product.images[0].url,
+      stock: product.stock,
+      seller: product.seller,
+    })
+  }
+
   return (
     <>
       <BreadCrumbs breadCrumbs={breadCrumbs} />
@@ -91,7 +109,11 @@ const ProductDetails = ({ product }) => {
               <p className="mb-4 text-gray-500">{product?.description}</p>
 
               <div className="flex flex-wrap gap-2 mb-5">
-                <button className="px-4 py-2 inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700">
+                <button className="px-4 py-2 inline-block text-white bg-blue-600 border border-transparent 
+                  rounded-md hover:bg-blue-700"
+                  onClick={addToCartHandler}
+                  disabled={!inStock}
+                >
                   <i className="fa fa-shopping-cart mr-2"></i>
                   Add to cart
                 </button>
