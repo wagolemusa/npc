245e823f145b7a107ae444af04ee911b05ea8a5a
wagolemusa <homiemusa@gmail.com>
2023-10-10 15:56:08 +0300

    Protecting routes

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,10 +1,10 @@
 import { createRouter } from 'next-connect';
 import dbConnect from "../../../backend/config/dbConnect";
 import {getProduct } from "../../../backend/controllers/productController"
+import onError from "../../../backend/middlewares/auth"
 
 
-
-const router = createRouter();
+const router = createRouter({ onError });
 
 dbConnect();
 
