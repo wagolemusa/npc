32419b0c63f5655e3dcd5361dfe9f69a57431d5b
wagolemusa <homiemusa@gmail.com>
2023-12-14 11:42:57 +0300

    fineshed webhook

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,4 +1,6 @@
+import getRawBody from "raw-body";
 import Stripe from "stripe";
+import Order from '../model/order'
 const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 export const checkoutSession = async (req, res) => {
@@ -41,4 +43,49 @@ export const checkoutSession = async (req, res) => {
   res.status(200).json({
     url: session.url,
   });
+};
+
+
+export const webhook = async (req, res) => {
+  try {
+    const rawBody = await getRawBody(req);
+    const signature = req.headers["stripe-signature"];
+
+    const event = stripe.webhooks.constructEvent(
+      rawBody,
+      signature,
+      // process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      const line_items = await stripe.checkout.sessions.listLineItems(
+        event.data.object.id
+      );
+
+      const orderItems = await getCartItems(line_items);
+      const userId = session.client_reference_id;
+      const amountPaid = session.amount_total / 100;
+
+      const paymentInfo = {
+        id: session.payment_intent,
+        status: session.payment_status,
+        amountPaid,
+        taxPaid: session.total_details.amount_tax / 100,
+      };
+
+      const orderData = {
+        user: userId,
+        shippingInfo: session.metadata.shippingInfo,
+        paymentInfo,
+        orderItems,
+      };
+
+      const order = await Order.create(orderData);
+      res.status(201).json({ success: true });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
